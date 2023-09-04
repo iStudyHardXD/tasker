7398dd91779476ca47d5114d3a83e024a813103d
iStudyHardXD <143990374+iStudyHardXD@users.noreply.github.com>
2023-09-04 15:33:09 +0400

    Add files via upload

---
+++ style/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace test;
+class test
+{
+    static void Main()
+    {
+        byte num_1 = 1;
+        Console.WriteLine("Минус " + num_1);
+        Console.ReadKey();
+
+    }
+
+}
+
+
+
+
+
+
